eb9622d7f21621282e44047d5cedbd460b78060b
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-02-08 22:04:56 +0530

    Update 847. Shortest Path Visiting All Nodes.cpp

--- Breadth First search/847. Shortest Path Visiting All Nodes.cpp
+++ Breadth First search/847. Shortest Path Visiting All Nodes.cpp
@@ -1,3 +1,13 @@
+/*Using bfs to solve this problem.
+
+Every node can have 2^n state because of mask and total number of nodes are n, therefore in worst case we will push n * 2^n states in queue and pop them.
+now for every state we will try to check it neighbors and see if we can push them.
+A node can have O(n) neighbors.
+hence:
+
+TC : O(n * n * 2^n)
+SC: O(n)
+*/
 class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
@@ -28,6 +38,8 @@ public:
         {
             auto z=q.front();
             q.pop();
+			
+			//all bits are set which means all nodes are finally visited
             if(z[2]==(1<<n)-1)
             {
                 ans=z[0];
