da637699aef8f77aa16dcb639a950f971d2dafe6
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-02-12 17:55:33 +0530

    Create 2150. Find All Lonely Numbers in the Array.cpp

---
+++ Leetcode Solution/2150. Find All Lonely Numbers in the Array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findLonely(vector<int>& nums) {
+        
+        unordered_map<int,int>m;
+        int i,n=nums.size(),c=0;
+        for(i=0;i<n;i++)
+        {
+            m[nums[i]]++;
+        }
+        set<int>s;
+        for(i=0;i<n;i++)
+        {
+            if(m[nums[i]]==1&&m[nums[i]-1]==0&&m[nums[i]+1]==0)
+            {
+                s.insert(nums[i]);
+            }
+        }
+        vector<int>ans(s.begin(),s.end());
+ 
+      
+        return ans;
+        
+    }
+};
+
+//TC - O(N)
+//SC - O(N)
