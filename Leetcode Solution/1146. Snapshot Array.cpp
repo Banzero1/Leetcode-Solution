1d9ba133ad17cd30c32864423ce4a4301250ce28
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-02-12 18:48:32 +0530

    Create 1146. Snapshot Array.cpp

---
+++ Leetcode Solution/1146. Snapshot Array.cpp
@@ -0,0 +1,37 @@
+class SnapshotArray {
+private:
+    map<int, map<int, int>> snaps;
+    int snapId = 0;
+public:
+    SnapshotArray(int length) {
+        for (int i = 0; i < length; i++) {
+            map<int, int> mp; 
+            mp[0] = 0;
+            snaps[i] = mp;
+        }
+    }
+    
+    void set(int index, int val) {
+        snaps[index][snapId] = val;
+    }
+    
+    int snap() {
+        return snapId++;
+    }
+    
+    int get(int index, int snap_id) {
+        auto it = snaps[index].upper_bound(snap_id); 
+        it--;
+        return it->second;
+    }
+};
+
+/*
+
+Time Complexity:
+Set: O(1) single operation
+Get: O(log n) for a single operation
+
+Space Complexity: O(n)
+
+*/
