d255ec82f7eb6f1f6ea73f4bb11dde7267591b94
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-03-09 21:47:54 +0530

    Time: 53 ms (16.47%), Space: 13.2 MB (88.47%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    
+    
+        bool kahnAlgo(vector<vector<int>> &adj, int n, vector<int> &indegree, vector<int> &ans)
+    {
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            // will push all the nodes with the indegree 0 in the queue as we have completed all the prerequisites for it.
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+        while (!q.empty())
+        {
+            // one by one we will take every element of the queue and will traverse the adjacency list of it 
+            // and will remove that node and will reduce the indegree of the adjacent nodes which are
+            // prerequisites for it.
+            int curr = q.front();
+            q.pop();
+
+            for (auto a : adj[curr])
+            {
+                indegree[a] -= 1;
+                // and will push the node having 0 indegree in the queue.
+                if (indegree[a] == 0)
+                    q.push(a);
+            }
+            // and after processing current node, will push it in the ans.
+            ans.push_back(curr);
+            count++;
+        }
+
+        // checking for DAG.
+        if (count != n)
+            return false;
+        return true;
+    }
+    
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        
+        int n = prerequisites.size();
+        vector<vector<int>> adj(numCourses); // Adjacency matrix.
+        vector<int> indegree(numCourses, 0); // indegree array.
+
+        // traversing all the nodes of the graph.
+        for (int i = 0; i < n; i++)
+        {
+            // filling adjacency matrix for all the nodes of the graph.
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            // also filling indegree value for all nodes.
+            indegree[prerequisites[i][0]] += 1;
+        }
+
+        vector<int> ans;
+
+        return kahnAlgo(adj, numCourses, indegree, ans);
+       
+        
+    }
+};
