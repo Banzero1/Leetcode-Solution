cbf19db34003a5cd652adf3ca91c3a70c5ee1535
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-03-17 09:46:59 +0530

    Time: 5 ms (14.59%), Space: 6.2 MB (45.05%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+       int scoreOfParentheses(string S) {
+        stack<int> stack;
+        int cur = 0;
+        for (char i : S)
+            if (i == '(') {
+                stack.push(cur);
+                cur = 0;
+            }
+            else {
+                cur += stack.top() + max(cur, 1);
+                stack.pop();
+            }
+        return cur;
+    }
+};
