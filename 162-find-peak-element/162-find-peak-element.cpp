dc3959689cf0b18a9909a59cc0d627352c60b446
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-03-23 05:57:28 +0530

    Time: 7 ms (42.54%), Space: 8.9 MB (5.18%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) 
+    {
+        int low = 0;
+        int high = num.size()-1;
+        
+        while(low < high)
+        {
+            int mid1,mid2;
+            mid1=(low+high)/2;
+            mid2=mid1+1;
+            if(num[mid1]<num[mid2])
+            {
+                low=mid2;
+            }
+            else
+            {
+                high=mid1;
+            }
+            
+            
+        }
+        return low;
+    }
+};
+
+//TC : O(logn)
+//SC : O(1)    
+
+/*
+
+In case of simple Binary Search, we work on a sorted sequence of numbers and try to find out the required number by reducing the search space at every step. In this case, we use a modification of this simple Binary Search to our advantage. We start off by finding the middle element, midmid from the given numsnums array. If this element happens to be lying in a descending sequence of numbers. or a local falling slope(found by comparing nums[i]nums[i] to its right neighbour), it means that the peak will always lie towards the left of this element. Thus, we reduce the search space to the left of midmid(including itself) and perform the same process on left subarray.
+
+If the middle element, midmid lies in an ascending sequence of numbers, or a rising slope(found by comparing nums[i]nums[i] to its right neighbour), it obviously implies that the peak lies towards the right of this element. Thus, we reduce the search space to the right of midmid and perform the same process on the right subarray.
+
+In this way, we keep on reducing the search space till we eventually reach a state where only one element is remaining in the search space. This single element is the peak element.
+
+This same logic can be applied to the left hand side (nums[x] < nums[x-1]).
+*/
