506ff5d771348fcc6201a297862d54b1e9f25595
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-03-25 21:45:42 +0530

    Time: 95 ms (68.64%), Space: 11.1 MB (75.27%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[10000 + 1][12 + 1];
+    
+    int memoization(vector<int>& wt, int w, int n)
+    {
+        if (n == 0 || w == 0)
+            return (w == 0) ? 0 : INT_MAX - 1;
+        
+        if (dp[w][n] != -1)
+            return dp[w][n];
+			
+        if (wt[n - 1] > w) 
+            return dp[w][n] = 0 + memoization(wt, w - 0, n - 1);
+        else 
+            return dp[w][n] = min(0 + memoization(wt, w - 0, n - 1), 1 + memoization(wt, w - wt[n - 1], n));
+    }
+    
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        memset(dp, -1, sizeof(dp));
+        int minCoins = memoization(coins, amount, coins.size());
+        return minCoins == INT_MAX - 1 ? -1 : minCoins;    
+    }
+};
