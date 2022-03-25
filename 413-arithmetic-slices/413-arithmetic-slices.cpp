06df4ebf6f4d45589e6c6ff2eb834c20d35b3b26
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-03-26 05:25:26 +0530

    Time: 8 ms (10.61%), Space: 7.4 MB (48.47%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& A) {
-        int n = A.size();
-        if (n < 3) return 0;
-        vector<int> dp(n, 0); // dp[i] means the number of arithmetic slices ending with A[i]
-        if (A[2]-A[1] == A[1]-A[0]) dp[2] = 1; // if the first three numbers are arithmetic or not
-        int result = dp[2];
-        for (int i = 3; i < n; ++i) {
-            // if A[i-2], A[i-1], A[i] are arithmetic, then the number of arithmetic slices ending with A[i] (dp[i])
-            // equals to:
-            //      the number of arithmetic slices ending with A[i-1] (dp[i-1], all these arithmetic slices appending A[i] are also arithmetic)
-            //      +
-            //      A[i-2], A[i-1], A[i] (a brand new arithmetic slice)
-            // it is how dp[i] = dp[i-1] + 1 comes
-            if (A[i]-A[i-1] == A[i-1]-A[i-2]) 
-                dp[i] = dp[i-1] + 1;
-            result += dp[i]; // accumulate all valid slices
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if(nums.size() <= 2)return 0;
+        int count = 0, ans = 0;
+        for(int i = 0; i < nums.size() - 2; i++){
+            if(nums[i+1] - nums[i] == nums[i+2] - nums[i+1]){
+                count++;
+            }
+            else{
+                ans += ((count * (count+1)) / 2);
+                count = 0;
+            }
+            
         }
-        return result;
+        if(count) ans += ((count * (count+1)) / 2);
+        return ans;
     }
 };
