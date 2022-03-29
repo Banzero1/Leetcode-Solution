efe5dfa84447b25780dbeaa4c8fb370d6c75898d
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-03-29 08:30:01 +0530

    Time: 66 ms (79.77%), Space: 39.6 MB (68.12%) - LeetHub

---
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int kConcatenationMaxSum(vector<int>& nums, int k) {
+        
+        long long int i,n=nums.size(),pre=0,s=0,sum=0,mod=1e9+7,ans=0,c=0;
+        
+        
+        for(i=0;i<n;i++)
+        {
+            c+=nums[i];
+            pre=max(pre,c);
+        }
+        c=0;
+        for(i=n-1;i>=0;i--)
+        {
+            c+=nums[i];
+            s=max(s,c);
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        
+        if(sum>=0)
+        {
+            ans=((k-2)*sum)%mod;
+        }
+        
+        
+       // cout<<pre<<s<<endl;
+        ans=(((pre>0?pre:0)+(s>0?s:0))%mod+ans%mod)%mod;
+        s=0;
+        c=0;
+        //cout<<ans;
+        for(int j=0;j<2&&j<k;j++){
+        for(i=0;i<n;i++)
+        {
+            c=c>=0?(c+nums[i]):nums[i];
+            
+            s=max(s,c);
+            c%=mod;
+            s%=mod;
+        }
+       //     cout<<c<<j<<endl;
+    }
+       // cout<<s<<endl;
+        
+        if(k==1)
+        {
+            return s;
+        }
+        s=(s>0)?s%mod:0;
+        
+        ans=max(ans,s);
+        ans=ans%mod;
+       
+            return ans>0?ans:0;
+        
+    }
+};
