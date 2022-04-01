f6511fd860abdc7d4a884a227c369c81fa761cdf
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-04-01 09:46:47 +0530

    Time: 320 ms (40.69%), Space: 338 MB (5.20%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -12,17 +12,26 @@ public:
 	int solve(string s, int l, int r){
 		if(l>=r) return dp[l][r] = 0;
 		if(dp[l][r]!=-1) return dp[l][r];
-		if(isPalindrome(s,l,r)) return dp[l][r] = 0;
+		if(dp1[l][r]) return dp[l][r] = 0;
 		int ans = INT_MAX;
 		for(int i=l;i<=r-1;i++){
-			if(isPalindrome(s,l,i)) ans = min(ans, 1+solve(s,i+1,r));
+			if(dp1[l][i]) ans = min(ans, 1+solve(s,i+1,r));
 		}
 		return dp[l][r] = ans;
 	}
 
 	int minCut(string s) {
 		dp.resize(s.size(),vector<int> (s.size(),-1));
-		dp1.resize(s.size(),vector<int> (s.size(),-1));
+		dp1.resize(s.size(),vector<int> (s.size(),0));
+        int N=s.size();
+      
+for (int i = N - 1; i >= 0; --i) {
+	for (int j = i; j < N; ++j) {
+		if (s[i] == s[j]) dp1[i][j] = ((i + 1 <= j - 1) ? dp1[i + 1][j - 1] : 1);
+		else dp1[i][j] = 0;
+	}
+}
+        
 		return solve(s,0,s.size()-1);
 	}
 };
