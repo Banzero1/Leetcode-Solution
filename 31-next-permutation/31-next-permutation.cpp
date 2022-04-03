b214a2416ff610c88c925b75f2eb68e2d7054ae6
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-04-03 10:10:09 +0530

    Time: 4 ms (73.50%), Space: 12.2 MB (37.25%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        
+        next_permutation(nums.begin(),nums.end());
+        
+        //return nums;
+        
+    }
+};
